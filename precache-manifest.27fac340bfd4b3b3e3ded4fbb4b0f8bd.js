b4cdac9aa97d8b1831bcb9efce1b306e9a029ec0
Gilang Nuril Haqim <gilangn89@gmail.com>
2019-01-14 15:34:37 +0800

    Updates

---
+++ precache-manifest.27fac340bfd4b3b3e3ded4fbb4b0f8bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6780e359a2a0bfb111d1",
+    "url": "/glgnrl.github.io/static/js/runtime~main.6780e359.js"
+  },
+  {
+    "revision": "9c8cda3a560639012f96",
+    "url": "/glgnrl.github.io/static/js/main.9c8cda3a.chunk.js"
+  },
+  {
+    "revision": "7c38c5876a92c3641c85",
+    "url": "/glgnrl.github.io/static/js/1.7c38c587.chunk.js"
+  },
+  {
+    "revision": "9c8cda3a560639012f96",
+    "url": "/glgnrl.github.io/static/css/main.099eb7bc.chunk.css"
+  },
+  {
+    "revision": "6ddfac2611fb33906895df34ef63f440",
+    "url": "/glgnrl.github.io/index.html"
+  }
+];
